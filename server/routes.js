3eae782c92481b69b25a815330a78b55ad8a436f
Sparshith <sparsh.pro@gmail.com>
2016-12-02 01:15:37 -0800

    Test

--- server/routes.js
+++ server/routes.js
@@ -18,12 +18,45 @@ module.exports = function (app) {
 
     app.get('/webhook', function (req, res) {
         if (req.query['hub.verify_token'] === 'snag') {
-            console.log("Same");
-            res.send(req.query['hub.challenge'])
+            return res.send(req.query['hub.challenge'])
         }
-        console.log("Yo Yo");
         res.send('Error, wrong token')
     });
+
+    app.post('/webhook', function (req, res) {
+        var data = req.body;
+
+        // Make sure this is a page subscription
+        if (data.object === 'page') {
+
+            // Iterate over each entry - there may be multiple if batched
+            data.entry.forEach(function(entry) {
+                var pageID = entry.id;
+                var timeOfEvent = entry.time;
+
+                // Iterate over each messaging event
+                entry.messaging.forEach(function(event) {
+                    if (event.message) {
+                        receivedMessage(event);
+                    } else {
+                        console.log("Webhook received unknown event: ", event);
+                    }
+                });
+            });
+
+            // Assume all went well.
+            //
+            // You must send back a 200, within 20 seconds, to let us know
+            // you've successfully received the callback. Otherwise, the request
+            // will time out and we will keep trying to resend.
+            res.sendStatus(200);
+        }
+    });
+
+    function receivedMessage(event) {
+        // Putting a stub for now, we'll expand it in the following steps
+        console.log("Message data: ", event.message);
+    }
 
     app.get('/api/me', ensureAuthenticated, function (req, res) {
         console.log(req.user);
