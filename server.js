b5268fcd28a547855c149718fbd291b43b82b764
Sparshith <sparsh.pro@gmail.com>
2016-12-02 00:00:27 -0800

    Back to Http

--- server.js
+++ server.js
@@ -35,21 +35,21 @@ if (app.get('env') === 'production') {
   });
 }
 require('./server/routes')(app);
-
+/*
 https.createServer({
   key: fs.readFileSync('server-key.pem'),
   cert: fs.readFileSync('server-crt.pem'),
   ca: fs.readFileSync('ca-crt.pem')
 }, app).listen(8080);
 console.log('Express server listening on port ' + app.get('port'));
+*/
 
-/*
 // Routes
 var server = http.createServer(app);
 
 server.listen(8080);
 console.log('Express server listening on port ' + app.get('port'));
-*/
+
 /*
 app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
